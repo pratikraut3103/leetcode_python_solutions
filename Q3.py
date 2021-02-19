2744efa7b0f9fe0e3d7556fb3a5a357374b95027
pratikraut3103 <pratikraut3103@gmailcom>
2021-02-20 01:51:25 +0530

    Longest Substring Without Repeating Characters

---
+++ Q3.py
@@ -0,0 +1,17 @@
+'''
+3. Longest Substring Without Repeating Characters
+'''
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mapSet = {}
+        start, result = 0, 0
+
+        for end in range(len(s)):
+        	if s[end] in mapSet:
+        		start = max(mapSet[s[end]], start)
+        	result = max(result, end-start+1)
+        	mapSet[s[end]] = end+1
+
+        return result 
